63301d90ebc9408ec91e048649b3090cef0e9097
darshimo <s4qedth5y@gmail.com>
2019-03-08 23:21:43 +0900

    linux用.bashrcの名前の変更とOSに合った.bashrcをインストールするよう変更

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ "$(uname)" = "Darwin" ]; then
+    ln -si ~/dotfiles/.bashrc.mac ~/.bashrc
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    ln -si ~/dotfiles/.bashrc.linux ~/.bashrc
+fi
+
 for file in $(ls -a | grep "^\.[^\.]"); do
-    [[ "$file" == ".git" ]] && continue
-    [[ "$file" == ".DS_Store" ]] && continue
+    [ "$file" == ".git" ] && continue
+    [ "$file" == ".DS_Store" ] && continue
+    [ "$file" == ".bashrc.mac" ] && continue
+    [ "$file" == ".bashrc.ubuntu" ] && continue
 
     ln -si ~/dotfiles/$file ~/$file
 done
